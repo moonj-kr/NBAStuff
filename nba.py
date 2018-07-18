9e912c54c39aed62fe663c17a5cda0405ad7e1d9
Jisook Moon <moonj.kr@gmail.com>
2018-07-18 22:18:32 +0900

    fixing

--- nba.py
+++ nba.py
@@ -1,23 +1,20 @@
 playByPlay="Play by Play Data Sample"
 eventCodes="Event Codes"
 lineUp="Game Lineup Data Sample"
-playlst=[]
-eventlst=[]
-lineuplst=[]
-
-
+playlst=[] #play by play
+eventlst=[] #events
+lineuplst=[] # lineup
 
 # reads files and stores to according lists
 def read(name,lst):
     file = open(name)
-    line = file.readline()
     line = file.readline().strip()
     while line != "":
         splitLine = line.split("\t")
         lst.append(splitLine)
         line = file.readline().strip()
+        print(line)
     file.close()
-
 
 def main():
     read(playByPlay,playlst)
@@ -28,8 +25,36 @@ def main():
     players=dict()
     for player in lineuplst[2]:
         players[player]=[lineuplst[3],0]
-    for event in playlst[2]:
-        if(event==1):
+
+    # adding plus/minus to players
+    scoringTeam = playlst[10] #scoring team id
+    for event in playlst:
+        print(event)
+        print("here")
+        if event[2] == '1':
+            # if there is a goal
+            print("true")
+            # find team id
+
+
+
+
+
+
+
+#       if(event==1):
+ #           for player,list in players:
+  #
+   #             print(player)
+    #            print("WEIOFFFFFFFFFFFFIEWOFIJEI")
+     #           print(list[0])
+      #          print(scoringTeam)
+       #         if list[0] == scoringTeam:
+        #            print(list)
+         #           list[1] += 1
+          #          print(list)
+           #         print("wut")
+            #pass
             #do plus/minus
 
 
